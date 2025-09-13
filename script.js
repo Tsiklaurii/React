c9a6e529e5eb5daa6927bed7d280c681b0ea4d87
tsiklaurii <tsiklaurinino321@gmail.com>
2025-09-13 11:40:24 +0400

    Task 2

---
+++ script.js
@@ -0,0 +1,38 @@
+// const str = "Re:educate";
+// console.log(str.split(":"));
+
+// let num = -5;
+// let result = num > 0 ? "positive" : num < 0 ? "Negative" : "zero";
+// console.log(result);
+
+// let sum = 0;
+// for (let i = 0; i <= 100; i++) {
+//     sum += i;
+// }
+// console.log("sum:", sum);
+
+
+// let tasks = ["task 1", "task 2", "task 3", "task 4", "task 5"];
+// console.log(tasks);
+
+// console.log(tasks[3]);
+
+// tasks.push("new task 6", "new task 7");
+// console.log(tasks);
+
+// let lastTask = tasks.pop();
+// console.log(lastTask)
+
+
+
+// let toDoList = [];
+// toDoList.push("task 1", "task 2", "task 3");
+// toDoList.unshift("urgent task");
+// toDoList.pop();
+// console.log(toDoList);
+
+
+let waitingWueue = ["person 1", "person 2", "person 3"];
+waitingWueue.push("person 4", "person 5");
+waitingWueue.shift()
+console.log(waitingWueue);
