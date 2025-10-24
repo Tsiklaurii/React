e9cfa8de65384fd9ba0b9fbb994217a912e6d38f
tsiklaurii <tsiklaurinino321@gmail.com>
2025-10-24 21:17:55 +0400

    feature/cards
    added cards

---
+++ React3/src/Cards.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export const InfoCard = (props) => {
+    return (
+        <div className='info_card'>
+            <div className='card'>
+                <h1>{props.title}</h1>
+                <p>{props.info}</p>
+                <a href={props.more}>Learn More</a>
+                <div className='underline'></div>
+            </div>
+        </div>
+    )
+}
+
+export const PhotoCard = (props) => {
+    return (
+        <div className='photo_card'>
+            <img src={props.img} />
+        </div>
+    )
+}
+
+export const InfoPhotoCard = (props) => {
+    return (
+        <div className='info_photo_card'>
+            <img src={props.img} />
+            <div className='info_photo_card_content'>
+                <h1 style={{ color: props.titleColor }}>{props.title}</h1>
+                <p style={{ color: props.infoColor }}>{props.info}</p>
+            </div>
+        </div>
+    )
+}
--- React3/src/Content.jsx
+++ React3/src/Content.jsx
@@ -1,11 +1,43 @@
 import React from 'react'
+import { InfoCard, InfoPhotoCard, PhotoCard } from './Cards'
 
 const Content = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <>
+            <div className='content'>
+                <InfoCard
+                    title="Transform your brand"
+                    info="We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you."
+                    more="#"
+                />
+                <PhotoCard
+                    img="src/assets/images/egg.jpg"
+                />
+                <PhotoCard
+                    img="src/assets/images/glass.jpg"
+                />
+                <InfoCard
+                    title="Stand out to the right audience"
+                    info="Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places."
+                    more="#"
+                />
+                <InfoPhotoCard
+                    img="src/assets/images/cherry.png"
+                    title="Graphic Design"
+                    titleColor="#24554A"
+                    info="Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention."
+                    infoColor="#3E7467"
+                />
+                <InfoPhotoCard
+                    img="src/assets/images/tangerine.jpg"
+                    title="Photography"
+                    titleColor="#052C3B"
+                    info="Increase your credibility by getting the most stunning, high-quality photos that improve your business image."
+                    infoColor="#19536C"
+                />
+            </div>
+        </>
+    )
 }
 
 export default Content
