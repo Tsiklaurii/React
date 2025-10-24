09250760bb41704d22af00c989b7f072d6ffd6c4
tsiklaurii <tsiklaurinino321@gmail.com>
2025-10-24 18:59:05 +0400

    refactor/header
    added cover photo

--- React3/src/Header.jsx
+++ React3/src/Header.jsx
@@ -2,13 +2,22 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div className='header'>
-            <img src="src/assets/images/logo.svg" alt="logo" className='logo' />
-            <div className='nav'>
-                <a href="#">About</a>
-                <a href="#">Services</a>
-                <a href="#">Projects</a>
-                <button className='contact_btn'>Contact</button>
+        <div className='header_container'>
+            <img src="src/assets/images/cover.png" alt="cover" className='cover' />
+
+            <div className='header_content'>
+                <div className='header'>
+                    <img src="src/assets/images/logo.svg" alt="logo" className='logo' />
+                    <div className='nav'>
+                        <a href="#">About</a>
+                        <a href="#">Services</a>
+                        <a href="#">Projects</a>
+                        <button className='contact_btn'>Contact</button>
+                    </div>
+                </div>
+
+                <h1 className="title">We are creatives</h1>
+                <img src="src/assets/images/arrow.svg" alt="arrow" className='arrow' />
             </div>
         </div>
     )
