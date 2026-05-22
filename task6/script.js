6334c5867466953491127afb2842eef46d711b3f
tsiklaurii <tsiklaurinino321@gmail.com>
2026-05-22 23:50:03 +0400

    fix: empty inputs

--- task6/script.js
+++ task6/script.js
@@ -18,15 +18,20 @@ const colorForm = document.querySelector(".colorForm");
 colorForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const inputValue = document.querySelector('input[name="color"]').value;
+    const input = document.querySelector('input[name="color"]');
+    const inputValue = input.value;
+
     const colors = ["red", "blue", "green", "black", "white"];
 
     if (!inputValue) {
         alert("Please input color");
+        input.value = "";
     } else if (!colors.includes(inputValue)) {
         alert("Please enter: red, blue, green, black or white");
+        input.value = "";
     } else {
         root.style.backgroundColor = inputValue;
+        input.value = "";
     }
 });
 
@@ -35,13 +40,17 @@ const numberForm = document.querySelector(".numberForm");
 numberForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const inputValue = document.querySelector('input[name="numbers"]').value;
+    const input = document.querySelector('input[name="numbers"]');
+    const inputValue = input.value;
+
     if (!inputValue) {
         alert("Please enter numbers");
+        input.value = "";
     } else {
         const numbers = inputValue.split(":").map((num) => Number(num));
         const sum = numbers.reduce((acc, curr) => acc + curr, 0);
         const average = sum / numbers.length;
         alert(`Average of ${inputValue} is: ${average}`);
+        input.value = "";
     }
 });
